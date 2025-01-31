2193a581b81e6dce902ed8999122bfdad07b6df5
Rukwaro <116838278+Ruks-7@users.noreply.github.com>
2025-01-31 12:15:26 +0300

    Finding out if an array has duplicate values

--- Arrays&Hashing/contains_duplicate.js
+++ Arrays&Hashing/contains_duplicate.js
@@ -4,7 +4,6 @@
  */
 
 //Solution 1
-//Using a nested loop results into a high time complexity thus not an optimal solution
 //Using set() and comparing the sizes of original array and Set object
 var containsDuplicate = function(nums) {
     let uniqueValues = new Set(nums)
@@ -16,10 +15,24 @@ var containsDuplicate = function(nums) {
     return (console.log(true))
 };
 
-containsDuplicate([1,2,3,4,5,6,7,8,7,10]) 
+//Solution 2
+//Using a nested loop results into a high time complexity thus not an optimal solution
+var containsDuplicate = function(nums) {
+    let length = nums.length
+
+    for(i=0; i<=length; i++){
+
+        for(a=i+1; a<=length; a++){
+            if(nums[i] === nums[a]){
+                return (console.log(true))
+            }
+        }
+    }
+    return (console.log(false))
+};
+
 
 //Get the duplicate values
-//Comment the code above (lines 9 to 19) to run the code below
 var containsDuplicate = function(nums) {
     let uniqueValues = new Set()
     duplicates=[]
@@ -36,4 +49,3 @@ var containsDuplicate = function(nums) {
 }
 
 containsDuplicate([1,2,3,4,5,6,7,8,7,10,9,2]) 
-
